117b21e2a15e9ba9a06e703e8ecc285aa2ff29a3
Lee Pavelich <leepavelich@gmail.com>
2022-01-17 19:31:20 +0000

    Removed legacy comment

--- typewriter.js
+++ typewriter.js
@@ -3,7 +3,7 @@ const typewriter = (sentence, typeDelay) => {
   for (const char of sentence) {
     setTimeout(() => {
       process.stdout.write(char);
-    }, delay); // <= 1s delay to make it noticeable. Can dial it down later.
+    }, delay);
     delay += typeDelay;
   }
   
